225288be8cc2eca1fca624dd87f4c26cdcc1e4e6
Sojan <sojansgithub@outlook.com>
2021-03-09 00:33:05 +0530

    Login page and register models

--- CatamaranBlazor/Models/LoginModel.cs
+++ CatamaranBlazor/Models/LoginModel.cs
@@ -1,8 +1,16 @@
-﻿namespace CatamaranBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatamaranBlazor.Models
 {
     public class LoginModel
     {
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "Please check the username entered.")]
         public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "Please check the password entered.")]
         public string Password { get; set; }
     }
 }
---
+++ CatamaranBlazor/Models/RegisterModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CatamaranBlazor.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "Please check the First Name.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "Please check the Last Name.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50, ErrorMessage = "Please check the Email address.")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [StringLength(50, ErrorMessage = "Please check the Date of Birth.")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(50, ErrorMessage = "Please check the Phone number.")]
+        public long PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "Please check the chosen user name.")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "Please check the password entered.")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(50, ErrorMessage = "Please check the reenterd user name.")]
+        public string Reenter_Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "Please check the reentered password.")]
+        public string Reenter_Password { get; set; }
+    }
+}
+
