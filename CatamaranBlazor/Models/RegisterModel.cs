59cd13435c9423d1bbc20bbe65eaa9ee8137bcc8
Sojan <sojansgithub@outlook.com>
2021-03-09 11:52:47 +0530

    Adding register form

--- CatamaranBlazor/Models/RegisterModel.cs
+++ CatamaranBlazor/Models/RegisterModel.cs
@@ -21,13 +21,10 @@ namespace CatamaranBlazor.Models
         public string EmailAddress { get; set; }
 
         [Required]
-        [DataType(DataType.Date)]
-        [StringLength(50, ErrorMessage = "Please check the Date of Birth.")]
         public DateTime DateOfBirth { get; set; }
 
         [Required]
         [DataType(DataType.PhoneNumber)]
-        [StringLength(50, ErrorMessage = "Please check the Phone number.")]
         public long PhoneNumber { get; set; }
 
         [Required]
@@ -40,10 +37,6 @@ namespace CatamaranBlazor.Models
         [StringLength(50, ErrorMessage = "Please check the password entered.")]
         public string Password { get; set; }
 
-        [Required]
-        [DataType(DataType.Text)]
-        [StringLength(50, ErrorMessage = "Please check the reenterd user name.")]
-        public string Reenter_Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
